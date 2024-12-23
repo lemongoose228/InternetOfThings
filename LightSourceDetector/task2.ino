1e494e6288717dc86e01a29107b43456755c5509
Екатерина <150834453+lemongoose228@users.noreply.github.com>
2024-12-23 18:28:33 +0800

    Update task2.ino

--- LightSourceDetector/task2.ino
+++ LightSourceDetector/task2.ino
@@ -1,32 +1,44 @@
-#define PHOTO_SENSOR_PIN  A4
+#define PHOTO_SENSOR_PIN  A1
 #define PHOTO_SENSOR_PIN2  A0
-#define LED_R 13
-#define LED_L 12
+#define LED_R 3
+#define LED_L 2
+
+unsigned long previousMillis = 0; 
+const long interval = 500;
 
 void setup() {
     Serial.begin(9600);
+    pinMode(LED_R, OUTPUT);
+    pinMode(LED_L, OUTPUT);
 }
 
 void loop() {
-    int photo_val = analogRead(PHOTO_SENSOR_PIN);
+    unsigned long currentMillis = millis(); 
+
+    int photo_val1 = analogRead(PHOTO_SENSOR_PIN);
     int photo_val2 = analogRead(PHOTO_SENSOR_PIN2);
     Serial.print("Photo val 1 = ");
-    Serial.println(photo_val);
+    Serial.println(photo_val1);
     Serial.print("Photo val 2 = ");
     Serial.println(photo_val2);
     delay(500);
 
-    if ((photo_val - photo_val2) >= 10){
-      digitalWrite(LED_L, LOW);
-      digitalWrite(LED_R, HIGH);
-      
-    }
-    else if((photo_val2 - photo_val) >= 10){
-      digitalWrite(LED_R, LOW);
-      digitalWrite(LED_L, HIGH);
-    }
+    if (photo_val1 > photo_val2 + 50) {
+        if (currentMillis - previousMillis >= interval) {
+            previousMillis = currentMillis;
+            digitalWrite(LED_R, !digitalRead(LED_R)); 
+            digitalWrite(LED_L, LOW);
+
+    } 
+    else if (photo_val2 > photo_val1 + 50) {
+        if (currentMillis - previousMillis >= interval) {
+            previousMillis = currentMillis;
+            digitalWrite(LED_L, !digitalRead(LED_L));
+            digitalWrite(LED_R, LOW); 
+        }
+    } 
     else{
-      digitalWrite(LED_R, LOW);
-      digitalWrite(LED_L, LOW);
+      digitalWrite(LED_R, HIGH);
+      digitalWrite(LED_L, HIGH);
     }
 }
